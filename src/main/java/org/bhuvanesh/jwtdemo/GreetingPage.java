3051b0808fa6c66187b7a7655ccb36c6c4950d1e
bhuvanesh-kp <bhuvaneshkpb@gmail.com>
2025-10-05 16:41:15 +0530

    RBC, hashing add

--- src/main/java/org/bhuvanesh/jwtdemo/GreetingPage.java
+++ src/main/java/org/bhuvanesh/jwtdemo/GreetingPage.java
@@ -1,13 +1,25 @@
 package org.bhuvanesh.jwtdemo;
 
-import org.springframework.stereotype.Service;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingPage {
-    @GetMapping("api/public")
+    @GetMapping("/hello")
     public String GetHello(){
         return "Hello World";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String GetUser(){
+        return "Hello user";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String GetAdmin(){
+        return "Hello admin";
     }
 }
